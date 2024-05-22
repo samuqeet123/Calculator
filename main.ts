3e3d469fc1f780c6fa88a596444d481b735755ea
samuqeet123 <sabdulmuqeetburki@gmail.com>
2024-05-22 12:37:35 +0500

    first commit

--- main.ts
+++ main.ts
@@ -1,26 +1,72 @@
 import inquirer from "inquirer"
 const answer = await inquirer.prompt([
     {
-        type: "input",
-        name: "GuessedNumberByProgram",
-        message: "Choise the number between 0 to"
+        type: "list",
+        name: "Question1",
+        message: "What is the capital city of Pakistan?",
+        choices: ["Karachi", "Islamabad", "Non of Them"]
     },
     {
-        type: "input",
-        name: "GuessTheNumber",
-        message: "Enter the guessed number"
+        type: "list",
+        name: "Question2",
+        message: "Who is the Prime Minister of Pakistan?",
+        choices: ["Nawaz Sharif", "Shahbaz Sharif", "Asim Munir"]
+
+    },
+    {
+        type: "list",
+        name: "Question3",
+        message: "What is the formula of water?",
+        choices: ["H2O", "CO2", "Non of Them"]
+    },
+    {
+        type: "list",
+        name: "Question4",
+        message: "Which one is the fruit from the following",
+        choices: ["Tomato", "Potato", "Non of Them"]
     }
 ])
-let guessed = parseInt(answer.GuessedNumberByProgram)
-let yourGuess = parseInt(answer.GuessTheNumber)
-// console.log(guessed)
-// console.log(yourGuess)
 
-let programGuessed = Math.floor(Math.random() * guessed)
+let Q1 = answer.Question1
+let score = 0
 
-if (programGuessed == yourGuess) {
-    console.log("You Win")
+if (Q1 == "Karachi") {
+    score += 1
 }
-else {
-    console.log(`You lose..The write answer was ${programGuessed}.. Try again`)
+else { score }
+
+let Q2 = answer.Question2
+if (Q2 == "Asim Munir") {
+    score += 1
+}
+else { score }
+
+let Q3 = answer.Question3
+if (Q3 == "H2O") {
+    score += 1
+}
+else { score }
+
+let Q4 = answer.Question4
+if (Q4 == "Tomato") {
+    score += 1
+}
+else { score }
+
+switch (score) {
+    case 4:
+        console.log("Excellent");
+        break;
+    case 3:
+        console.log("Very Good");
+        break;
+    case 2:
+        console.log("Keep it up");
+        break;
+    case 1:
+        console.log("Nice try");
+        break;
+    default :
+        console.log("letter luck next time");
+        break;
 }
